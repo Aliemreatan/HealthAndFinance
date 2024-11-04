dbf459ff2925071a6ff6e9011484bb42e62583c8
Ali Emre Atan <aliemreatan@hotmail.com>
2024-11-04 18:47:03 +0300

    Add project files.

---
+++ HealthAndFinance/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HealthAndFinance.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
