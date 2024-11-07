0aed17ad6da491b962b7603b15b78b66f5e8ba59
Ali Emre Atan <aliemreatan@hotmail.com>
2024-11-07 23:33:19 +0300

    HealthAdd

---
+++ ClassLibrary1/ValidationRules/HealthEntryValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using HealthAndFinance.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthAndFinance.Business.ValidationRules
+{
+    public class HealthEntryValidator : AbstractValidator<HealthEntry>
+    {
+       public HealthEntryValidator()
+        {
+            RuleFor(x => x.Height).NotEmpty().WithMessage("Uzunluk kısmını boş bırakmayınız.");
+            RuleFor(x => x.Weight).NotEmpty().WithMessage("Kilo kısmını boş bırakmayınız.");
+            RuleFor(x => x.SleepHours).NotEmpty().WithMessage("Haftalık uyuma saatinizi boş bırakmayınız.");
+        }
+    }
+}
---
+++ HealthAndFinance/Controllers/UserWeightController.cs
@@ -0,0 +1,70 @@
+﻿using HealthAndFinance.Business.Concrete;
+using HealthAndFinance.Business.ValidationRules;
+using HealthAndFinance.DataAccess.Concrete;
+using HealthAndFinance.DataAccess.EntityFrameWork;
+using HealthAndFinance.Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+
+namespace HealthAndFinance.WebUI.Controllers
+{
+    public class UserWeightController : Controller
+    {
+        HealthEntryManager hem = new HealthEntryManager(new EfHealthEntryRepository());
+        Context c = new Context();
+
+        public IActionResult Index()
+        {
+            var username = User.Identity.Name;
+
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+
+            var userid = c.Users.Where(x => x.Email == usermail).Select(y => y.Email).FirstOrDefault();
+
+
+            ViewBag.Name = username;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult HealthAdd()
+        {
+            List<SelectListItem> healthvalues = (from x in hem.GetList()
+                                                 select new SelectListItem
+                                                 {
+                                                     Text = x.User.NameSurname,
+                                                     Value = x.UserId.ToString()
+                                                 }).ToList();
+            ViewBag.hv = healthvalues;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult HealthAdd(HealthEntry p)
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(x => x.Email).FirstOrDefault();
+            var userID = c.Users.Where(x => x.Email == usermail).Select(y => y.Id).FirstOrDefault();
+
+            HealthEntryValidator hv = new HealthEntryValidator();
+            FluentValidation.Results.ValidationResult results = hv.Validate(p);
+            if (results.IsValid)
+            {
+                p.BKI = p.Weight / p.Height * 2;
+                p.Date = DateTime.Now;
+                hem.TAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.PropertyName);
+                }
+            }
+            return View();
+        }
+    }
+}
